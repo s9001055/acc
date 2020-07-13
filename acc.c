0ab729558c392cf7600471093a2867c1895ce70b
Arthur Chang <s9001055@gmail.com>
2020-07-13 14:27:21 +0800

    can compiler add and subtraction

--- acc.c
+++ acc.c
@@ -1,5 +1,5 @@
 /* 
-* This program can compiler a INT c code 
+* This program can compiler add and subtraction c code 
 * 
 * Author: 	Arthur Chang
 * date:		2020/07/13
@@ -15,10 +15,30 @@ int main(int argc, char **argv)
 		return 1;
 	}
 
+	char *p = argv[1];
+
 	printf(".intel_syntax noprefix\n");
 	printf(".global main\n");
 	printf("main:\n");
-	printf("  mov rax, %d\n", atoi(argv[1]));
+	printf("  mov rax, %ld\n", strtol(p, &p, 10));
+
+	while (*p){
+		if (*p == '+'){
+			p++;
+			printf("  add rax, %ld\n",strtol(p, &p, 10));
+			continue;
+		}
+		if (*p == '-'){
+			p++;
+			printf("  sub rax, %ld\n",strtol(p, &p, 10));
+			continue;
+			}
+		
+		fprintf(stderr, "out of expectation character: '%c'\n", *p);
+		return 1;
+	}
+
+
 	printf("  ret\n");
 	return 0;
 }
